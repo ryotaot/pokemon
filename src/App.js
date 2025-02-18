35457a487aa480740ea726c8c248dc83353a32cc
ryota <ryota200309@email.com>
2025-02-18 18:24:33 +0900

    api取得

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
+import { getAllPokemon } from "./utils/Pokemon.js";
 
 function App() {
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData();
+  }, []);
   return <div className="App"></div>;
 }
 
