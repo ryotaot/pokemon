f7bd9f2c1f554166b8d5adfd84e4030d6bdbc2bf
ryota <ryota200309@email.com>
2025-02-19 12:47:34 +0900

    次へボタンの実装

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import { getAllPokemon, getPokemon } from "./utils/Pokemon.js";
 import Card from "./components/Card.js";
+import Navbar from "./components/Navbar/Navbar.js";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setloading] = useState(true);
   const [_pokemonData, setPokemonData] = useState([]);
+  const [nextURL, setNextURL] = useState("");
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -14,7 +16,7 @@ function App() {
       let res = await getAllPokemon(initialURL);
       /*各ポケモンの詳細なデータを取得*/
       loadPokemon(res.results);
-
+      setNextURL(res.next);
       setloading(false);
     };
     fetchPokemonData();
@@ -31,20 +33,36 @@ function App() {
   };
   console.log(_pokemonData);
 
+  const handleNextPage = async () => {
+    setloading(true);
+    let data = await getAllPokemon(nextURL);
+    await loadPokemon(data.results);
+    setloading(false);
+  };
+
+  const handlePrevPage = () => {};
+
   return (
-    <div className="App">
-      {loading ? (
-        <h1>ロード中</h1>
-      ) : (
-        <>
-          <div className="ContpokemonCardainer">
-            {_pokemonData.map((pokemon, index) => {
-              return <Card key={index} pokemon={pokemon} />;
-            })}
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {loading ? (
+          <h1>ロード中</h1>
+        ) : (
+          <>
+            <div className="ContpokemonCardainer">
+              {_pokemonData.map((pokemon, index) => {
+                return <Card key={index} pokemon={pokemon} />;
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={handlePrevPage}>前へ</button>
+              <button onClick={handleNextPage}>次へ</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
